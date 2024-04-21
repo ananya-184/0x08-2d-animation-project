2da0962929846db535eac85032c3dbbc5ac1580b
Ananya Gupta <ananyagupta184@gmail.com>
2024-04-21 21:27:03 +0530

    modified the files

--- experiment.js
+++ experiment.js
@@ -1,30 +1,19 @@
 class Experiment {
   // Group Details
-  static rollNos = '10983437,10983743'
-  static names = 'The Tutors(Akhtar Banga, Phul Tekchand)'
+  static rollNos = '102103435,102103444,102283009'
+  static names = 'Flappy Fiskers(Armaan Saggu, Reyyan Ahmed, Ananya Gupta)'
 
-  canvasSel = '#myCanvas'
+  canvasSel = '#board'
 
   run() {
 
     // Run the Steppers
-    // this.runSteppers()
+    this.runSteppers()
 
     // Hide Steppers
-    this.hideSteppers()
-    canvasSetup(this.canvasSel)
+    // this.hideSteppers()
+    // canvasSetup(this.canvasSel)
 
-    // Clock
-    // --------------------------------------------------
-    const clock = new Clock(this.canvasSel)
-    // const ms = document.timeline.currentTime
-    // clock.draw(ms)
-    // clock.draw(ms+25000)
-    const clockRafFn = (ts) => {
-      clock.draw(ts)
-      window.requestAnimationFrame(clockRafFn)
-    }
-    const clockRaf = window.requestAnimationFrame(clockRafFn)
     
   }
 
